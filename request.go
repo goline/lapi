c9a8e246f2c83c39ae89df747a5fd60bad643a7b
Long Do <long.dotrong@lazada.com>
2017-08-23 17:23:56 +0700

    Update Request, Route

--- request.go
+++ request.go
@@ -2,35 +2,11 @@ package lapi
 
 // Request represents for an application's request
 type Request interface {
-	RequestIdentifier
-	RequestVersioner
 	RequestResolver
-	RequestInput
-}
-
-// RequestVersioner handles multiple versions
-type RequestVersioner interface {
-	// Version returns version string, e.g, v1, v1.1
-	Version() string
-}
-
-// RequestIdentifier identifies a request
-type RequestIdentifier interface {
-	// Method returns request's method
-	Method() string
-
-	// Uri returns request's path
-	Uri() string
 }
 
 // RequestResolver returns routing information
 type RequestResolver interface {
 	// Route returns matched route for request
 	Route() Route
-}
-
-// RequestInput contains request's input
-type RequestInput interface {
-	// Input returns an instance of Bag
-	Input() Bag
 }
--- route.go
+++ route.go
@@ -2,12 +2,27 @@ package lapi
 
 // Route acts a route describer
 type Route interface {
+
+	RouteHandler
+}
+
+type RouteIdentifier interface {
+	// Name returns route's name
+	Name() string
+}
+
+type RouteDescriber interface {
 	// Method returns HTTP Method string
 	Method() string
 
 	// Uri gives HTTP Uri
 	Uri() string
 
+	// Version returns API version
+	Version() string
+}
+
+type RouteHandler interface {
 	// Handler shows Handler of this route
 	Handler() Handler
 }
