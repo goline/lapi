070d1b690fb105b5b48d7e8be62e277c4e13303e
Long Do <me@dotronglong.com>
2017-09-10 09:07:25 +0700

    Change file name from rescue.go to rescuer.go
    Change error handler test to rescuer test

--- rescuer_test.go
+++ rescuer_test.go
@@ -5,58 +5,58 @@ import (
 	"testing"
 )
 
-func TestNewErrorHandler(t *testing.T) {
-	h := NewErrorHandler()
+func TestNewRescuer(t *testing.T) {
+	h := NewRescuer()
 	if h == nil {
 		t.Errorf("Expects h is not nil")
 	}
 }
 
-func TestFactoryErrorHandler_HandleError_HandleSystemError_HttpNotFound(t *testing.T) {
+func TestFactoryRescuer_Rescue_HandleSystemError_HttpNotFound(t *testing.T) {
 	c := NewConnection(nil, &FactoryResponse{})
 	e := NewSystemError(ERROR_HTTP_NOT_FOUND, "")
-	h := &FactoryErrorHandler{}
-	h.HandleError(c, e)
+	h := &FactoryRescuer{}
+	h.Rescue(c, e)
 	if c.Response().Status() != http.StatusNotFound {
 		t.Errorf("Expects http status is StatusNotFound. Got %d", c.Response().Status())
 	}
 }
 
-func TestFactoryErrorHandler_HandleError_HandleSystemError_HttpBadRequest(t *testing.T) {
+func TestFactoryRescuer_Rescue_HandleSystemError_HttpBadRequest(t *testing.T) {
 	c := NewConnection(nil, &FactoryResponse{})
 	e := NewSystemError(ERROR_HTTP_BAD_REQUEST, "")
-	h := &FactoryErrorHandler{}
-	h.HandleError(c, e)
+	h := &FactoryRescuer{}
+	h.Rescue(c, e)
 	if c.Response().Status() != http.StatusBadRequest {
 		t.Errorf("Expects http status is StatusBadRequest. Got %d", c.Response().Status())
 	}
 }
 
-func TestFactoryErrorHandler_HandleError_HandleSystemError_Default(t *testing.T) {
+func TestFactoryRescuer_Rescue_HandleSystemError_Default(t *testing.T) {
 	c := NewConnection(nil, &FactoryResponse{})
 	e := NewSystemError(9999, "")
-	h := &FactoryErrorHandler{}
-	h.HandleError(c, e)
+	h := &FactoryRescuer{}
+	h.Rescue(c, e)
 	if c.Response().Status() != http.StatusInternalServerError {
 		t.Errorf("Expects http status is StatusInternalServerError. Got %d", c.Response().Status())
 	}
 }
 
-func TestFactoryErrorHandler_HandleError_HandleStackError(t *testing.T) {
+func TestFactoryRescuer_Rescue_HandleStackError(t *testing.T) {
 	c := NewConnection(nil, &FactoryResponse{})
 	e := NewStackError(http.StatusInternalServerError, []Error{NewError("11", "err1", nil)})
-	h := &FactoryErrorHandler{}
-	h.HandleError(c, e)
+	h := &FactoryRescuer{}
+	h.Rescue(c, e)
 	if c.Response().Status() != http.StatusInternalServerError {
 		t.Errorf("Expects http status is StatusInternalServerError. Got %d", c.Response().Status())
 	}
 }
 
-func TestFactoryErrorHandler_HandleError_UnknownError(t *testing.T) {
+func TestFactoryRescuer_Rescue_UnknownError(t *testing.T) {
 	c := NewConnection(nil, &FactoryResponse{})
 	e := NewError("11", "err1", nil)
-	h := &FactoryErrorHandler{}
-	h.HandleError(c, e)
+	h := &FactoryRescuer{}
+	h.Rescue(c, e)
 	if c.Response().Status() != http.StatusInternalServerError {
 		t.Errorf("Expects http status is StatusInternalServerError. Got %d", c.Response().Status())
 	}
@@ -67,20 +67,20 @@ type myUnknownError struct{}
 func (e *myUnknownError) Error() string { return "" }
 func (e *myUnknownError) Status() int   { return http.StatusInternalServerError }
 
-func TestFactoryErrorHandler_HandleError_UnknownErrorWithStatus(t *testing.T) {
+func TestFactoryRescuer_Rescue_UnknownErrorWithStatus(t *testing.T) {
 	c := NewConnection(nil, &FactoryResponse{})
 	e := &myUnknownError{}
-	h := &FactoryErrorHandler{}
-	h.HandleError(c, e)
+	h := &FactoryRescuer{}
+	h.Rescue(c, e)
 	if c.Response().Status() != http.StatusInternalServerError {
 		t.Errorf("Expects http status is StatusInternalServerError. Got %d", c.Response().Status())
 	}
 }
 
-func TestFactoryErrorHandler_HandleError_NotHandle(t *testing.T) {
+func TestFactoryRescuer_Rescue_NotHandle(t *testing.T) {
 	e := &myUnknownError{}
-	h := &FactoryErrorHandler{}
-	err := h.HandleError(nil, e)
+	h := &FactoryRescuer{}
+	err := h.Rescue(nil, e)
 	if err == nil {
 		t.Errorf("Expects err is nil")
 	}
