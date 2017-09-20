1a69e3f35568c932c313663202eb50282c41f796
Long Do <me@dotronglong.com>
2017-09-20 15:28:49 +0700

    Improve resolve struct

--- container.go
+++ container.go
@@ -181,8 +181,12 @@ func (c *FactoryContainer) resolveStruct(at reflect.Type, args ...interface{}) (
 	if ok == false {
 		return nil, NewSystemError(ResolveErrorNotExistAbstract, fmt.Sprintf("%v is not bound yet", at))
 	}
-
-	return value.Elem().Interface(), nil
+	switch value.Kind() {
+	case reflect.Ptr:
+		return value.Interface(), nil
+	default:
+		return value.Elem().Interface(), nil
+	}
 }
 
 func (c *FactoryContainer) structOf(value interface{}) (reflect.Type, SystemError) {
